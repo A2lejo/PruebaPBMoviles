731f56955799c3022ca811f55684c0952d752570
Alejandro <alejandro27campoverde@gmail.com>
2024-10-14 17:51:46 -0500

    Cambios

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
 
 defineCustomElements(window);
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.log(err));
