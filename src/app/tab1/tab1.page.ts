6a9d0469ae23a8e7c073d544153e0464cb59a100
Alejandro <alejandro27campoverde@gmail.com>
2024-10-21 17:28:15 -0500

    Practica para la prueba

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -2,11 +2,34 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  templateUrl: './tab1.page.html',
+  styleUrls: ['./tab1.page.scss'],
 })
 export class Tab1Page {
+  actual: string = '';
+  buttons: string[] = [
+    '7', '8', '9', '/',
+    '4', '5', '6', '*',
+    '1', '2', '3', '-',
+    '0', '.', '=', '+',
+    'C'
+  ];
 
-  constructor() {}
+  buttonClicked(button: string) {
+    if (button === '=') {
+      this.calculate();
+    } else if (button === 'C') {
+      this.actual = '';
+    }else {
+      this.actual += button;
+    }
+  }
 
+  calculate() {
+    try {
+      this.actual = eval(this.actual);
+    } catch (e) {
+      this.actual = 'Error';
+    }
+  }
 }
